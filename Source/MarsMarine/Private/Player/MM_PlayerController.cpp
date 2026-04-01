d36efc31a7c2382fe8ddd91be6027091a40f6328
umikami <ggoku02@gmail.com>
2026-04-01 11:42:27 -0400

    Added C++ player controller to setup default input mapping context

---
+++ Source/MarsMarine/Private/Player/MM_PlayerController.cpp
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/MM_PlayerController.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputMappingContext.h"
+
+
+void AMM_PlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	
+	if (IsLocalPlayerController())
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(DefaultMappingContext, 0);
+		}
+	}
+}
---
+++ Source/MarsMarine/Public/Player/MM_PlayerController.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "MM_PlayerController.generated.h"
+
+/**
+ * 
+ */
+
+class UInputMappingContext;
+
+
+UCLASS()
+class MARSMARINE_API AMM_PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+protected:
+	UPROPERTY(EditAnywhere, Category ="Input|Input Mappings")
+	TObjectPtr<UInputMappingContext> DefaultMappingContext;
+	
+	virtual void SetupInputComponent() override;
+};
