330ee1197cde3c893dbd7068c5df1845e9834899
umikami <ggoku02@gmail.com>
2026-04-02 10:09:03 -0400

    Added base class for alien AI enemy

---
+++ Source/MarsMarine/Private/Pawns/MM_AlienBase.cpp
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Pawns/MM_AlienBase.h"
+
+
+// Sets default values
+AMM_AlienBase::AMM_AlienBase()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AMM_AlienBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMM_AlienBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void AMM_AlienBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
---
+++ Source/MarsMarine/Public/Pawns/MM_AlienBase.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "MM_AlienBase.generated.h"
+
+UCLASS()
+class MARSMARINE_API AMM_AlienBase : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AMM_AlienBase();
+	
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	
+};
