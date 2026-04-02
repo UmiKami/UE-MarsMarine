92292c3368d7e35ee191a17e57080a342861886d
umikami <ggoku02@gmail.com>
2026-04-02 09:06:54 -0400

    Added mouse look around and other binding movement inputs

--- Source/MarsMarine/Private/Pawns/MM_CharacterBase.cpp
+++ Source/MarsMarine/Private/Pawns/MM_CharacterBase.cpp
@@ -1,37 +1,111 @@
-
-
 #include "Pawns/MM_CharacterBase.h"
+#include "EnhancedInputComponent.h"
+#include "NiagaraComponent.h"
+#include "Blueprint/UserWidget.h"
+#include "Components/AudioComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "Player/MM_PlayerController.h"
+#include "UI/MM_UserWidgetBase.h"
 
 // Sets default values
 AMM_CharacterBase::AMM_CharacterBase()
 {
 	PrimaryActorTick.bCanEverTick = true;
+}
 
+void AMM_CharacterBase::SetupPlayerHUD()
+{
+	UUserWidget* PlayerHUD = CreateWidget<UUserWidget>(GetWorld(), PlayerHUDClass);
+	UMM_UserWidgetBase* MarineUserWidgetHUD = Cast<UMM_UserWidgetBase>(PlayerHUD);
+	
+	MarineUserWidgetHUD->SetMarineCharacter(this);
+	MarineUserWidgetHUD->AddToViewport();
 }
 
 void AMM_CharacterBase::BeginPlay()
 {
 	Super::BeginPlay();
-	
-}
-
-void AMM_CharacterBase::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
 
 	if (!NiagaraMuzzleFlash)
 	{
 		NiagaraMuzzleFlash = FindComponentByTag<UNiagaraComponent>("MuzzleFlash");
 	}
+	
+	SetupPlayerHUD();
 }
 
 void AMM_CharacterBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComponent->BindAction(MoveForwardBackwardAction, ETriggerEvent::Triggered, this, &ThisClass::MoveForwardsBackwards);
+		EnhancedInputComponent->BindAction(MoveLeftRightAction, ETriggerEvent::Triggered, this, &ThisClass::MoveLeftRight);
 		EnhancedInputComponent->BindAction(FireAction, ETriggerEvent::Started, this, &ThisClass::StartFiringWeapon);
 		EnhancedInputComponent->BindAction(FireAction, ETriggerEvent::Completed, this, &ThisClass::StopFiringWeapon);
+	}
+}
+
+void AMM_CharacterBase::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	LookAtCursorPosition(DeltaSeconds);
+}
+
+
+void AMM_CharacterBase::MoveForwardsBackwards(const FInputActionValue& Value)
+{
+	const float ScaleValue = Value.Get<float>();
+
+	AddMovementInput({1, 0, 0}, ScaleValue);
+}
+
+void AMM_CharacterBase::MoveLeftRight(const FInputActionValue& Value)
+{
+	const float ScaleValue = Value.Get<float>();
+
+	AddMovementInput({0, 1, 0}, ScaleValue);
+}
+
+void AMM_CharacterBase::LookAtCursorPosition(float DeltaTime)
+{
+	if (!MPlayerController)
+	{
+		MPlayerController = Cast<AMM_PlayerController>(GetController());
+	};
+
+	if (!MPlayerController) return;
+
+	float PositionX = 0.f;
+	float PositionY = 0.f;
+
+	MPlayerController->GetMousePosition(PositionX, PositionY);
+
+	FVector2D MousePosition = {PositionX, PositionY};
+
+	FVector WorldPosition = {};
+	FVector WorldDirection = {};
+
+	UGameplayStatics::DeprojectScreenToWorld(MPlayerController, MousePosition, WorldPosition, WorldDirection);
+
+	const FVector EndPoint = WorldPosition + WorldDirection * 2000;
+
+	FHitResult OutHit;
+	GetWorld()->LineTraceSingleByChannel(OutHit, WorldPosition, EndPoint, ECC_Visibility);
+
+	FRotator LookAtRotation = UKismetMathLibrary::FindLookAtRotation(GetActorLocation(), OutHit.Location);
+
+	FRotator LookAtAmount = UKismetMathLibrary::RInterpTo(
+		MPlayerController->GetControlRotation(),
+		LookAtRotation,
+		DeltaTime,
+		MouseSmoothingInterpSpeed
+	);
+
+	MPlayerController->SetControlRotation({0, LookAtAmount.Yaw, 0});
 }
 
 void AMM_CharacterBase::StartFiringWeapon(const FInputActionValue& Value)
--- Source/MarsMarine/Public/Pawns/MM_CharacterBase.h
+++ Source/MarsMarine/Public/Pawns/MM_CharacterBase.h
@@ -6,6 +6,11 @@
 #include "GameFramework/Character.h"
 #include "MM_CharacterBase.generated.h"
 
+struct FInputActionValue;
+class UInputAction;
+class AMM_PlayerController;
+class UNiagaraComponent;
+
 UCLASS()
 class MARSMARINE_API AMM_CharacterBase : public ACharacter
 {
@@ -14,16 +19,30 @@ class MARSMARINE_API AMM_CharacterBase : public ACharacter
 public:
 	AMM_CharacterBase();
 
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	virtual void Tick(float DeltaSeconds) override;
+	
+	UFUNCTION(BlueprintCallable)
+	FORCEINLINE float GetHealth() const {return Health;}
+	
+	UFUNCTION(BlueprintCallable)
+	FORCEINLINE int32 GetEnemiesKilledCount() const {return EnemiesKilled;}
+
 protected:
 	virtual void BeginPlay() override;
-
-public:	
-	virtual void Tick(float DeltaTime) override;
-
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
+	
+	UPROPERTY(EditAnywhere, Category="Input")
+	UInputAction* MoveForwardBackwardAction;
+	
+	UPROPERTY(EditAnywhere, Category="Input")
+	UInputAction* MoveLeftRightAction;
+	
 	UPROPERTY(EditAnywhere, Category="Input")
 	UInputAction* FireAction;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Input")
+	float MouseSmoothingInterpSpeed = 1.f;
+	
 	UPROPERTY(VisibleAnywhere, Category="VFX")
 	TObjectPtr<UNiagaraComponent> NiagaraMuzzleFlash;
 	
@@ -32,8 +51,28 @@ public:
 	
 	UPROPERTY(EditDefaultsOnly, Category="SFX")
 	TObjectPtr<USoundBase> EndRifleShotSound;
+	
+	UPROPERTY(EditDefaultsOnly, Category="UI")
+	TSubclassOf<UUserWidget> PlayerHUDClass;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Player Stats")
+	float Health;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Player Stats")
+	int32 EnemiesKilled;
+	
+private:
+	UPROPERTY()
+	TObjectPtr<AMM_PlayerController> MPlayerController;
+	
 	UPROPERTY()
 	TObjectPtr<UAudioComponent> SpawnedRifleShotAudioComponent;
+	
+	void MoveForwardsBackwards(const FInputActionValue& Value);
+	void MoveLeftRight(const FInputActionValue& Value);
+	void LookAtCursorPosition(float DeltaTime);
 	void StartFiringWeapon(const FInputActionValue& Value);
 	void StopFiringWeapon(const FInputActionValue& Value);
+	
+	void SetupPlayerHUD();
 };
