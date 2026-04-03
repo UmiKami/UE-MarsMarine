1ad39f1b87bbe9b850517923ec8d8c5b0b2ceb23
umikami <ggoku02@gmail.com>
2026-04-03 10:56:29 -0400

    attempt to add debug symbols on linux

--- Source/MarsMarine.Target.cs
+++ Source/MarsMarine.Target.cs
@@ -11,5 +11,6 @@ public class MarsMarineTarget : TargetRules
 		DefaultBuildSettings = BuildSettingsVersion.V5;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_6;
 		ExtraModuleNames.Add("MarsMarine");
+		bCreateMapFile = true;
 	}
 }
