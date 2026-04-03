692c75872f83b1c7940a5c0af06f95937ef39720
umikami <ggoku02@gmail.com>
2026-04-03 17:59:24 -0400

    Added wave count and methods to GameMode

--- Source/MarsMarine/Private/Game/MM_MarsGameMode.cpp
+++ Source/MarsMarine/Private/Game/MM_MarsGameMode.cpp
@@ -2,3 +2,22 @@
 
 
 #include "Game/MM_MarsGameMode.h"
+
+void AMM_MarsGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	GetWorldTimerManager().SetTimer(GameStartDelayTimer, this, &ThisClass::SetupNewWave, StartDelay, false);
+}
+
+void AMM_MarsGameMode::SetupNewWave()
+{
+	
+}
+
+FString AMM_MarsGameMode::DisplayCurrentWave()
+{
+	FString DisplayMessage = FString::Printf(TEXT("Wave %d"), CurrentWave);
+	
+	return DisplayMessage;
+}
--- Source/MarsMarine/Public/Game/MM_MarsGameMode.h
+++ Source/MarsMarine/Public/Game/MM_MarsGameMode.h
@@ -13,4 +13,21 @@ UCLASS()
 class MARSMARINE_API AMM_MarsGameMode : public AGameMode
 {
 	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable)
+	FString DisplayCurrentWave();
+	
+protected:
+	virtual void BeginPlay() override;
+	
+	UFUNCTION()
+	void SetupNewWave();
+	
+	UPROPERTY(EditDefaultsOnly)
+	float StartDelay = 2.f;
+	
+	int32 CurrentWave = 1;
+	
+	FTimerHandle GameStartDelayTimer;
 };
